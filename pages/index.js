1af4fb58190acc05fed28ea1329eb37c2d5be245
SorinC6 <kish.sorin@yahoo.com>
2022-08-17 14:17:34 +0300

    add banner and sanity client

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import { Product, FooterBanner, HeroBanner } from "../components";
 const Home = () => {
   return (
     <>
-      HeroBanner
+      <HeroBanner />
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Tech of many variations</p>
@@ -12,7 +12,7 @@ const Home = () => {
       <div className="products-container">
         {["product1", "product2"].map((item) => item)}
       </div>
-      Footer
+      <FooterBanner />
     </>
   );
 };
